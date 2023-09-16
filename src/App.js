7211bb205e5084af937682dfc3b91c866c7b968f
Yuqiao (Simon) Su <Simon.Suyq@gmail.com>
2023-09-16 18:50:56 -0400

    main page

--- src/App.js
+++ src/App.js
@@ -4,31 +4,31 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      {/*<header className="App-header">*/}
-      {/*  <img src={logo} className="App-logo" alt="logo" />*/}
-      {/*  <p>*/}
-      {/*    Edit <code>src/App.js</code> and save to reload.*/}
-      {/*  </p>*/}
-      {/*  <a*/}
-      {/*    className="App-link"*/}
-      {/*    href="https://reactjs.org"*/}
-      {/*    target="_blank"*/}
-      {/*    rel="noopener noreferrer"*/}
-      {/*  >*/}
-      {/*    Learn React*/}
-      {/*  </a>*/}
-      {/*</header>*/}
-      <ul>
-        <li><a href="/kanbas/profile/index.html">Account</a></li>
-        <li><a href="#">Dashboard</a></li>
-        <li><a href="/kanbas/home.html">Courses</a></li>
-        <li><a href="#">Calendar</a></li>
-        <li><a href="#">Inbox</a></li>
-        <li><a href="#">History</a></li>
-        <li><a href="#">Studio</a></li>
-        <li><a href="#">Commons</a></li>
-        <li><a href="#">Help</a></li>
-      </ul>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      {/*<ul>*/}
+      {/*  <li><a href="/kanbas/profile/index.html">Account</a></li>*/}
+      {/*  <li><a href="#">Dashboard</a></li>*/}
+      {/*  <li><a href="/kanbas/home.html">Courses</a></li>*/}
+      {/*  <li><a href="#">Calendar</a></li>*/}
+      {/*  <li><a href="#">Inbox</a></li>*/}
+      {/*  <li><a href="#">History</a></li>*/}
+      {/*  <li><a href="#">Studio</a></li>*/}
+      {/*  <li><a href="#">Commons</a></li>*/}
+      {/*  <li><a href="#">Help</a></li>*/}
+      {/*</ul>*/}
     </div>
   );
 }
