170c623c347fc0c3f11296b6e70f068a3520b83b
yuqiaosu-toast <Simon.Suyq@gmail.com>
2023-10-11 21:15:32 -0400

    A3Lab

--- src/App.js
+++ src/App.js
@@ -1,36 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Labs from "./Labs";
+import HelloWorld from "./Labs/a3/HelloWorld";
+import Kanbas from "./Kanbas";
+import {HashRouter} from "react-router-dom";
+import {Routes, Route, Navigate} from "react-router";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      {/*<ul>*/}
-      {/*  <li><a href="/kanbas/profile/index.html">Account</a></li>*/}
-      {/*  <li><a href="#">Dashboard</a></li>*/}
-      {/*  <li><a href="/kanbas/home.html">Courses</a></li>*/}
-      {/*  <li><a href="#">Calendar</a></li>*/}
-      {/*  <li><a href="#">Inbox</a></li>*/}
-      {/*  <li><a href="#">History</a></li>*/}
-      {/*  <li><a href="#">Studio</a></li>*/}
-      {/*  <li><a href="#">Commons</a></li>*/}
-      {/*  <li><a href="#">Help</a></li>*/}
-      {/*</ul>*/}
-    </div>
-  );
+    return (
+        <HashRouter>
+            <div>
+                <Routes>
+                    <Route path="/"         element={<Navigate to="/Labs"/>}/>
+                    <Route path="/Labs/*"   element={<Labs/>}/>
+                    <Route path="/hello"    element={<HelloWorld/>}/>
+                    <Route path="/kanbas/*" element={<Kanbas/>}/>
+                </Routes>
+            </div>
+        </HashRouter>
+    );
 }
-
 export default App;
---
+++ src/Labs/a3/DynamicStyling.js
@@ -0,0 +1,13 @@
+import JavaScript from "./JavaScript";
+import PathParameters from "./PathParameters";
+import Classes from "./Classes";
+
+function DynamicStyling(){
+    return (
+        <div>
+            <h1>Dynamic Styling</h1>
+            <Classes/>
+        </div>
+    );
+}
+export default DynamicStyling;
