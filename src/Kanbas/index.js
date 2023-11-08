507a3fc614ce76663eac1ac6b8d7efdb03e91858
yuqiaosu-toast <Simon.Suyq@gmail.com>
2023-11-08 16:57:22 -0500

    almost finished

--- src/Kanbas/index.js
+++ src/Kanbas/index.js
@@ -14,7 +14,9 @@ function Kanbas() {
         name: "New Course",      number: "New Number",
         startDate: "2023-09-10", endDate: "2023-12-15",
     });
-    const URL = "http://localhost:4000/api/courses";
+    const API_BASE = process.env.REACT_APP_API_BASE;
+    console.log(API_BASE);
+    const URL = `${API_BASE}/courses`;
     const findAllCourses = async () => {
         const response = await axios.get(URL);
         setCourses(response.data);
